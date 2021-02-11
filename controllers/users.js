258825afb38394a4cdcb320e1d84d4cf73d1431b
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-10 20:56:47 -0800

    bcrypt password

--- controllers/users.js
+++ controllers/users.js
@@ -1,30 +1,34 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
-exports.getLogin = (req, res, next) =>{
-    User.find({})
-    .then(res => {
-        console.log(res);
-    })
-    .catch(err=>{
-        console.log(err);
-    })
-}
-exports.postLogin = (req, res, next )=>{
+// exports.getLogin = (req, res, next) =>{
+//     User.find({})
+//     .then(res => {
+//         console.log(res);
+//     })
+//     .catch(err=>{
+//         console.log(err);
+//     })
+// }
+exports.postRegister = (req, res, next) => {
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
 
-    const user = new User({
-        username:username,
-        email:email,
-        password:password
-    });
-    user.save()
-    .then(result =>{
-        console.log(result);
-    })
-    .catch(err => {
-        console.log(err);
-    })
+    bcrypt.hash(password, 12)
+        .then(hashpassword => {
+            const user = new User({
+                username: username,
+                email: email,
+                password: hashpassword
+            });
+           return user.save();
+        })
+        .then(result => {
+            // console.log(result);
+        })
+        .catch(err => {
+            console.log(err);
+        })
 
 }
